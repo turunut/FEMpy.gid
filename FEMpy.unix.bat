b2beddf61742fa3cee90ccdc3ce256a31e43c385
turunut <fturon@hotmail.com>
2023-05-16 07:03:23 +0200

    initial

---
+++ FEMpy.unix.bat
@@ -0,0 +1,7 @@
+#!/bin/sh
+# Basename: $1
+# Directory: $2
+# prblFolderctory: $3
+# OutputFile: "$1.log"
+# ErrorFile: "ErrMsg.dat"
+
---
+++ FEMpy.win.bat
@@ -0,0 +1,5 @@
+rem Basename: %1
+rem Directory: %2
+rem prblFolderctory: %3
+rem OutputFile: %1.log
+rem ErrorFile: ErrMsg.dat
